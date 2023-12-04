037c334d6ffe7855a1ff536f94ac1572041a6d44
carla <carlaff96@gmail.com>
2023-12-04 10:52:55 +0100

    Primer commit con las variables de python

---
+++ 01_variables.py
@@ -0,0 +1,10 @@
+#Crear una variable en python
+nombre = "Carla" #tipo string
+numero1 = 10 #tipo numero entero
+numero2 = 10.5 #tipo decimal
+booleano1 = True
+booleano2 = False
+
+#Print es una funcion que muestra información por el terminal
+print(nombre)
+print(type(nombre))
